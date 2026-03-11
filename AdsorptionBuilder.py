cb6685a21296b27b877936f7217cd82bf661058d
acabezon-nextmol <alfonso.cabezon@nextmol.com>
2026-03-11 12:50:42 +0100

    Fixed dimensions and cleaning files

--- AdsorptionBuilder.py
+++ AdsorptionBuilder.py
@@ -24,6 +24,8 @@ import MDAnalysis as mda
 import subprocess
 import argparse
 import numpy as np
+import os
+import sys
 
 # =============================================================================
 # Variables
@@ -31,7 +33,7 @@ import numpy as np
 
 z_mix = 195
 z_sol = 24
-w_mass = 76
+w_mass = 72
 polymer_fraction = 0.09
 
 # =============================================================================
@@ -67,12 +69,77 @@ def run_gmx(cmd : list) -> str:
     
     return result.stdout
 
+def write_dummy_mdp(file_name="dummy.mdp"):
+    """Writes a dummy .mdp file to generate .tprs
+
+    Parameters
+    ----------
+    file_name : str, optional
+        _description_, by default "dummy.mdp"
+    """    
+    mdp_content = """integrator               = steep
+nsteps                   = 100
+
+nstxout                  = 0
+nstvout                  = 0
+nstfout                  = 0
+
+cutoff-scheme            = Verlet
+nstlist                  = 20
+nsttcouple               = 20
+nstpcouple               = 20
+rlist                    = 1.35
+verlet-buffer-tolerance  = -1
+ns_type                  = grid
+pbc                      = xyz
+
+coulombtype              = reaction-field
+rcoulomb                 = 1.1
+epsilon_r                = 15\t; 2.5 (with polarizable water)
+epsilon_rf               = 0
+vdw_type                 = cutoff
+vdw-modifier             = Potential-shift-verlet
+rvdw                     = 1.1
+
+constraints              = none
+constraint_algorithm     = Lincs
+lincs_order              = 8
+lincs_warnangle          = 90
+lincs_iter               = 2
+"""
+    with open(file_name, "w") as f:
+        f.write(mdp_content)
+    
+def write_surface_top(surface_itp : str, file_name : str = "surface.top"):
+    """Writes a .top file for the surface 
+
+    Parameters
+    ----------
+    surface_itp : str
+        _description_
+    file_name : str, optional
+        _description_, by default "surface.top"
+    """    
+    top_content=f"""#include "martini_v3.0.0.itp"
+#include "{surface_itp}"
+
+[ system ]
+; name
+CG surface
+
+[ molecules ]
+; name         number
+  CG_surface   1
+"""
+    with open(file_name, "w+") as top:
+        top.write(top_content)
+
 def build_system(surface, polymer_gro, polymer_mass, polymer_charge, x, y, water_gro, gmx_bin):
     # Create the mixture of solvent + polymer
     cmd = [
         gmx_bin, "solvate",
         "-cs", water_gro,
-        "-box", str(x), str(y), str(int(z_mix/10)) ,
+        "-box", str(x/10), str(y/10), str(z_mix/10) ,
         "-o", "tmp_1.gro"
     ]
     solvate = run_gmx(cmd)
@@ -84,8 +151,9 @@ def build_system(surface, polymer_gro, polymer_mass, polymer_charge, x, y, water
         gmx_bin, "insert-molecules",
         "-f", "tmp_1.gro",
         "-ci", polymer_gro,
-        "-nmol", polymer_number,
+        "-nmol", str(polymer_number),
         "-radius", "0.21",
+        "-replace", "W",
         "-o", "tmp_2.gro"
     ]
     mixture = run_gmx(cmd)
@@ -94,7 +162,7 @@ def build_system(surface, polymer_gro, polymer_mass, polymer_charge, x, y, water
     cmd = [
         gmx_bin, "solvate",
         "-cs", water_gro,
-        "-box", str(x), str(y), str(int(z_sol/10)) ,
+        "-box", str(x/10), str(y/10), str(z_sol/10) ,
         "-o", "tmp_3.gro"
     ]
     buffer = run_gmx(cmd)
@@ -102,17 +170,21 @@ def build_system(surface, polymer_gro, polymer_mass, polymer_charge, x, y, water
 
     # Combine buffer and mixture and add counter ions
     buffer_max_z = np.max(u_buffer.atoms.positions[:, -1])
-    u_mix.atoms.positions = np.array([0.0, 0.0, buffer_max_z]) # displace mixture
+    u_mix.atoms.positions += np.array([0.0, 0.0, buffer_max_z + 2]) # displace mixture with a safety buffer
+    z_dim = buffer_max_z + u_mix.dimensions[2] + 2# Box dim in Z for merged universe
     buffer_mix = mda.Merge(u_buffer.atoms, u_mix.atoms) # Merge system
+    buffer_mix.dimensions = np.array([x, y, z_dim, 90.0, 90.0, 90.0])
+    buffer_mix.atoms.write("mix_no_ions.gro")
     
     surface_charge = np.sum(surface.atoms.charges)
-    system_charge = surface_charge + polymer_charge
+    system_charge = surface_charge + polymer_tot_charge
+    system_charge = int(system_charge)
 
-    if system_charge > 0:
+    if system_charge < 0:
         ion_resname = "ION"
         ion_name = "NA"
         charged = True
-    elif system_charge < 0:
+    elif system_charge > 0:
         ion_resname = "ION"
         ion_name = "CL"
         charged = True
@@ -124,25 +196,27 @@ def build_system(surface, polymer_gro, polymer_mass, polymer_charge, x, y, water
         replace_index = np.random.choice(waters.indices, size = abs(system_charge), replace = False)
         for idx in replace_index:
             atom = buffer_mix.atoms[idx]
-            atom.resname = ion_resname
+            atom.residue.resname = ion_resname
             atom.name = ion_name
     
-    z_dim = np.max(buffer_mix.atoms.positions[:, -1])
-    # Separate systems and merge to reorder and put polymer, then water, and the ions
+    z_dim = buffer_mix.dimensions[2]
+    # Separate systems and merge to reorder and put polymer, then water, and then ions
     polymers = buffer_mix.select_atoms("not resname W ION")
     waters = buffer_mix.select_atoms("resname W") 
     ions = buffer_mix.select_atoms("resname ION")
     ordered_u = mda.Merge(polymers, waters, ions)
     ordered_u.dimensions = np.array([x, y, z_dim, 90.0, 90.0, 90.0])
+    ordered_u.atoms.write("tmp_4.gro")
 
 
     # Mix three components
     z_surface = surface.dimensions[2]
-    ordered_u.atoms.positions += np.array([0.0, 0.0, z_surface]) # Displace mix
+    ordered_u.atoms.positions += np.array([0.0, 0.0, z_surface + 2]) # Displace mix and add safety buffer
 
     system = mda.Merge(surface.atoms, ordered_u.atoms) # Merge system
-    system.dimensions = np.array([x, y, z_dim + z_surface, 90.0, 90.0, 90.0])
-    system.atoms.write("final_system")
+    system.dimensions = np.array([x, y, z_dim + z_surface + 2, 90.0, 90.0, 90.0])
+    system.atoms.write("final_system.gro")
+    os.system("rm \#* tmp* mix*")
 
 
 
@@ -175,8 +249,13 @@ def main():
                         action = "store", type = str, 
                         metavar = f"{"<str>":<10}{"PATH":>15}",
                         help = "PATH to the gmx executable")
+    parser.add_argument("-r", "--restart", dest = "restart", required = False,
+                        action = "store_true",
+                        help = "ONLY IF surface.tpr EXIST")
     args = parser.parse_args()
 
+    # Set environment variables
+    os.environ["GMXLIB"] = "/home/acabezon-nextmol/forcefields/martini_v300"
     # Assign inputs to variables
     surface_gro = args.surface_gro
     surface_top = args.surface_top
@@ -185,8 +264,20 @@ def main():
     water_gro = args.water_gro
     gmx_bin = args.gmx_bin
 
+    if not args.restart:
+        write_dummy_mdp()
+        write_surface_top(surface_itp = surface_top)
+        cmd = [
+            gmx_bin, "grompp",
+            "-p", "surface.top",
+            "-c", surface_gro,
+            "-f", "dummy.mdp",
+            "-o", "surface"
+        ]
+        run_gmx(cmd)
     # Read surface and get simensions
-    surface = mda.Universe(surface_top, surface_gro, topology_format = "ITP")
+    surface = mda.Universe("surface.tpr", surface_gro)
+    # surface = mda.Universe(surface_top, surface_gro, topology_format = "ITP")
     dimensions = surface.dimensions
     x, y = dimensions[0], dimensions[1]
 
